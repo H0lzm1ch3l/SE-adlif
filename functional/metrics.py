0c425056fa84fde175065384b56286dd1b911af7
rferrand <romain.ferrand@tugraz.at>
2025-01-20 19:38:39 +0100

    add si-snr + refactor metric computation

--- functional/metrics.py
+++ functional/metrics.py
@@ -1,7 +1,12 @@
 import torch
-from torchmetrics.metric import Metric
-
-
+from torchmetrics import MeanSquaredError
+class MeanSquaredErrorFlat(MeanSquaredError):
+    def __init__(self, squared = True, num_outputs = 1, **kwargs):
+        super().__init__(squared, num_outputs, **kwargs)
+    def update(self, preds, target):
+        preds = preds.reshape(-1)
+        target = target.reshape(-1)
+        return super().update(preds, target)
 
 
 def spike_probability_metric_per_layer(
