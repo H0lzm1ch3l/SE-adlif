42c5629ceed66d84227885c67b25b5873a31bbd2
rferrand <romain.ferrand@tugraz.at>
2025-01-28 18:37:26 +0100

    correct conversion bug pytorch-jax

--- eval_model.py
+++ eval_model.py
@@ -27,17 +27,15 @@ def repair_checkpoint(in_state_dict):
 def get_decay(dt: float, alpha_min, alpha_max, w):
     import jax
     coef = jax.nn.sigmoid(w)
-    return alpha_max * coef + (1.0  - coef)*alpha_min
+    return alpha_max * coef + (1.0  - coef)*alpha_min 
 
 def adlif_pytorch_states_dict_to_jax_dict(ckpt_path):
     import jax
     import jax.numpy as jnp
-    from jax import Array
     ckpt = torch.load(ckpt_path, map_location=None if torch.cuda.is_available() else torch.device('cpu'))
     ckpt_state_dict = ckpt['state_dict']
     ckpt_state_dict = repair_checkpoint(ckpt_state_dict)
     q = ckpt['hyper_parameters']['cfg']['encoder']['l1']['q']
-    print(q)
     nb_bottleneck_neurons = ckpt['hyper_parameters']['cfg']['bottleneck_neurons']
     ckpt_state_dict = jax.tree.map(lambda x: jnp.array(x.cpu().numpy(), dtype=jnp.float32), ckpt_state_dict)
     enc_l1_decay_u = get_decay(1.0, 
@@ -110,8 +108,8 @@ def adlif_pytorch_states_dict_to_jax_dict(ckpt_path):
     factor = (ckpt_state_dict['loss.batch_count'] - transition_begin) / transition_steps
     min_temp = ckpt_state_dict['loss.min_temp']
     temp = ckpt_state_dict['loss.temp']
-    ckpt_state_dict['loss.temp'] = 10
-    print(ckpt_state_dict['loss.temp'])
+    ckpt_state_dict['loss.temp_'] = jnp.maximum(min_temp, temp*temp_decay**factor)
+    print(ckpt_state_dict['loss.temp_'])
     ckpt_state_dict.update(
         {
             "model.encoder.l1.decay_u": enc_l1_decay_u,
@@ -153,6 +151,7 @@ def se_adlif_model(model, weights: dict, inputs):
     
     enc_l1_a = weights['model.encoder.l1.a']
     enc_l1_b = weights['model.encoder.l1.b']
+    
     enc_l2_a = weights['model.encoder.l2.a']
     enc_l2_b = weights['model.encoder.l2.b']
     
@@ -177,6 +176,7 @@ def se_adlif_model(model, weights: dict, inputs):
     
     q = weights['q']
     nb_bottleneck_neurons = weights['nb_bottleneck_neurons']        
+    print(inputs.shape)
     def loop(carry, inputs):
         enc_l1_u, enc_l1_w, enc_l2_u, enc_l2_w, enc_l2_z, dec_l1_u, dec_l1_w, dec_l1_z, dec_l2_u, dec_l2_w, dec_l2_z, lout_u = carry
         cur = inputs @ weights['model.encoder.l1.weight'].T + weights['model.encoder.l1.bias']
@@ -205,10 +205,19 @@ def se_adlif_model(model, weights: dict, inputs):
         
         cur = dec_l2_z @ weights['model.decoder.out_layer.weight'].T + weights['model.decoder.out_layer.bias']
         lout_u = lout_decay * lout_u + (1.0 - lout_decay) * cur
-        probs = jax.nn.softmax(lout_u/weights['loss.temp'], -1)
+        probs = jax.nn.softmax(lout_u/weights['loss.temp_'], -1)
         out = inverse_A_law(jnp.sum(probs * weights['loss.bin_edges'], keepdims=True))
-        return (enc_l1_u, enc_l1_w, enc_l2_u, enc_l2_w, enc_l2_z, dec_l1_u, dec_l1_w, dec_l1_z, dec_l2_u, dec_l2_w, dec_l2_z, lout_u), out
-    _, out = jax.lax.scan(loop, (enc_l1_u0, enc_l1_w0, enc_l2_u0, enc_l2_w0, enc_l2_z0, dec_l1_u0, dec_l1_w0, dec_l1_z0, dec_l2_u0, dec_l2_w0, dec_l2_z0, lout_u),
+        return (enc_l1_u, enc_l1_w, 
+                enc_l2_u, enc_l2_w, enc_l2_z, 
+                dec_l1_u, dec_l1_w, dec_l1_z, 
+                dec_l2_u, dec_l2_w, dec_l2_z, 
+                lout_u), out
+    _, out = jax.lax.scan(loop, 
+                          (enc_l1_u0, enc_l1_w0, 
+                           enc_l2_u0, enc_l2_w0, enc_l2_z0, 
+                           dec_l1_u0, dec_l1_w0, dec_l1_z0, 
+                           dec_l2_u0, dec_l2_w0, dec_l2_z0, 
+                           lout_u),
                  inputs, unroll=1)
     return out
     
@@ -226,25 +235,18 @@ def inverse_A_law(y, a: float = 87.6):
 def adlif_dataloder_loop(model, weights, prediction_delay, wave_list_sorted, save_to):
     import jax
     import jax.numpy as jnp
-    from torchmetrics.audio import ScaleInvariantSignalNoiseRatio
-    import numpy as np
     save_to = Path(save_to)
     save_to.mkdir(exist_ok=True, parents=True)
     model = partial(se_adlif_model, model, weights)
-    model = jax.jit(jax.vmap(model), donate_argnums=0,)
+    model = jax.jit(jax.vmap(model),)
     i = 0
-    m = ScaleInvariantSignalNoiseRatio()
     for wave in tqdm(wave_list_sorted, "evaluation"):
-        _inputs, _ = torchaudio.load(wave)
-        inputs = jnp.array(_inputs, dtype=jnp.float32,).T
-        inputs = inputs[None, ...]
+        _inputs = torchaudio.load(wave)[0].T.unsqueeze(0)
+        inputs = jnp.array(_inputs.cpu().numpy(), dtype=jnp.float32,)
         inputs = jnp.concat((inputs, jnp.zeros((inputs.shape[0], prediction_delay, inputs.shape[2]), dtype=inputs.dtype)), axis=1)
         out = model(inputs)
-        print(out.shape)
-        # jnp.save(save_to/str(i), out[0, prediction_delay:], allow_pickle=False)
-        print(m(torch.from_numpy(np.array(out[0, prediction_delay:])), _inputs.T))
+        jnp.save(save_to/str(i), out[:, prediction_delay:], allow_pickle=False)
         i += 1
-        
 
 def create_test_libri_dataloader(ckpt_path, test_dataset_dir):
     ckpt = torch.load(ckpt_path, map_location=None if torch.cuda.is_available() else torch.device('cpu'))
