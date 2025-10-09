ca9198cc2b76401f11d2acabdb2a3851f2ee6e5b
Michael Siegl <michael.siegl@pro2future.at>
2025-10-09 14:14:18 +0200

    fixed sech implementaiton, in fact I had implemented its first derivation

--- sech.py
+++ sech.py
@@ -14,15 +14,19 @@ class sech(torch.autograd.Function):
         grad_input = grad_output * (sech_x**2 - sech_x * torch.tanh(x))
         return grad_input
     
+# def sech_fn(x):
+#     return - torch.sinh(x) / (torch.cosh(x)**2)
+
 def sech_fn(x):
-    return - torch.sinh(x) / (torch.cosh(x)**2)
+    return 1 / torch.cosh(x)
 
 # sech experiments:
-amplitude = 5.0
-omega = 2.0
-x = torch.linspace(-5, 5, steps=100)
+amplitude = 1.0
+omega = 3
+x = torch.linspace(-10, 10, steps=100)
 x.requires_grad = True
 y = amplitude * sech_fn(x / omega)
+# y = sech_fn(x)
 dy_dx = torch.autograd.grad(y, x, torch.ones_like(y), create_graph=True)
 
 data_x = x.detach().numpy()
