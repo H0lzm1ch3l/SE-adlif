16c4b0682539973f78abe411839aa2151aeb5b1a
michael.siegl <michael.siegl@pro2future.at>
2025-12-08 15:09:49 +0000

    small change to shellscript so that it corerctly executes all experiments at once but without a syntax error in the last run of the last experiment

--- exp_res.sh
+++ exp_res.sh
@@ -2,13 +2,18 @@
 
 experiments=("SSC_M1CLIF" "SSC_M2CLIF" "SSC_M3CLIF")
 runs=5
-gpu_count=8  # Number of available GPUs
+gpu_count=7  # Number of available GPUs
+gpu_offset=1
 
 for exp in "${experiments[@]}"; do
     for ((i=1; i<=runs; i++)); do
-        gpu_id=$(( (i - 1) % gpu_count ))
+        gpu_id=$(( (i - 1 + gpu_offset) % gpu_count ))
         echo "Running $exp - Iteration $i on GPU $gpu_id"
-        uv run run.py experiment=$exp device=cuda:$gpu_id
+        if [[ "$exp" == "${experiments[-1]}" ]] && [[ $i -eq $runs ]]; then
+            uv run run.py experiment=$exp device=cuda:$gpu_id
+        else
+            uv run run.py experiment=$exp device=cuda:$gpu_id &
+        fi
     done
 done
 
