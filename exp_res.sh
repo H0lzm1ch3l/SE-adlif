0caacf16bf80528eeedd6a9b1b5b31c44cc329b6
michael.siegl <michael.siegl@pro2future.at>
2025-12-08 11:43:33 +0000

    added newest tonic version

--- exp_res.sh
+++ exp_res.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-experiments=("ECG_M1CLIF" "ECG_M2CLIF" "ECG_M3CLIF" "SHD_M1CLIF" "SHD_M2CLIF" "SHD_M3CLIF" "SSC_M1CLIF" "SSC_M2CLIF" "SSC_M3CLIF")
+experiments=("SSC_M1CLIF" "SSC_M2CLIF" "SSC_M3CLIF")
 runs=5
 gpu_count=8  # Number of available GPUs
 
