eef35fbfe1690025c82296460d7f114ab3420d03
michael.siegl <michael.siegl@pro2future.at>
2025-12-11 10:40:08 +0000

    added experiment configuration files for ECG and SSC datasets for MCadlif

--- exp_res.sh
+++ exp_res.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# ( "ECG_MC1adLIF" "ECG_MC2adLIF" "ECG_MC3adLIF" "SHD_MC1adLIF" "SHD_MC2adLIF" "SHD_MC3adLIF" "SSC_MC1adLIF" "SSC_MC2adLIF" "SSC_MC3adLIF" )
-experiments=("ECG_M1CLIF" "ECG_M2CLIF" "ECG_M3CLIF" "SHD_M1CLIF" "SHD_M2CLIF" "SHD_M3CLIF" "SSC_M1CLIF" "SSC_M2CLIF" "SSC_M3CLIF")
+# experiments=("ECG_MC1adLIF" "ECG_MC2adLIF" "ECG_MC3adLIF" "SHD_MC1adLIF" "SHD_MC2adLIF" "SHD_MC3adLIF" "SSC_MC1adLIF" "SSC_MC2adLIF" "SSC_MC3adLIF")
+# experiments=("ECG_M1CLIF" "ECG_M2CLIF" "ECG_M3CLIF" "SHD_M1CLIF" "SHD_M2CLIF" "SHD_M3CLIF" "SSC_M1CLIF" "SSC_M2CLIF" "SSC_M3CLIF")
+experiments=("SSC_M1CLIF" "SSC_M2CLIF" "SSC_M3CLIF")
 runs=5
-gpu_count=8  # Number of available GPUs
-gpu_offset=0
+gpu_count=3  # Number of available GPUs
+gpu_offset=5
 
 for exp in "${experiments[@]}"; do
     for ((i=1; i<=runs; i++)); do
