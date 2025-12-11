21917af917cb1ddcd360bccf444d901af3eddbc0
michael.siegl <michael.siegl@pro2future.at>
2025-12-11 13:27:33 +0000

    added ecg and ssc configs

--- exp_res.sh
+++ exp_res.sh
@@ -2,14 +2,14 @@
 
 # experiments=("ECG_MC1adLIF" "ECG_MC2adLIF" "ECG_MC3adLIF" "SHD_MC1adLIF" "SHD_MC2adLIF" "SHD_MC3adLIF" "SSC_MC1adLIF" "SSC_MC2adLIF" "SSC_MC3adLIF")
 # experiments=("ECG_M1CLIF" "ECG_M2CLIF" "ECG_M3CLIF" "SHD_M1CLIF" "SHD_M2CLIF" "SHD_M3CLIF" "SSC_M1CLIF" "SSC_M2CLIF" "SSC_M3CLIF")
-experiments=("SSC_M1CLIF" "SSC_M2CLIF" "SSC_M3CLIF")
+experiments=("SSC_MC1adLIF" "SSC_MC2adLIF" "SSC_MC3adLIF")
 runs=5
 gpu_count=3  # Number of available GPUs
-gpu_offset=5
+gpu_offset=4
 
 for exp in "${experiments[@]}"; do
     for ((i=1; i<=runs; i++)); do
-        gpu_id=$(( (i - 1 + gpu_offset) % gpu_count ))
+        gpu_id=$(( ((i - 1) + gpu_offset) % gpu_count ))
         echo "Running $exp - Iteration $i on GPU $gpu_id"
         if [[ $i -lt $runs ]]; then
             uv run run.py experiment=$exp device=cuda:$gpu_id &
