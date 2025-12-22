96381ddcc5c057d59e79e817985a612cc3285060
Michael Siegl <michael.siegl98@gmx.at>
2025-12-22 23:36:56 +0100

    specified python patch version, data corruption issue still seems to be there, changed init of u_p to uniform with gain of u_p from 0 to bound fan_out (also try fan_in because fan_out is just the number of compartments which could screw the forward pass)

--- models/mclif_v2.py
+++ models/mclif_v2.py
@@ -86,10 +86,10 @@ class MCLIF2(Module):
         # maybe calculate gain from cfg u_p value
         if cfg.get('train_u_p', True):
             self.u_p = Parameter(torch.empty((self.out_features, self.num_compartments), **factory_kwargs))
-            torch.nn.init.xavier_normal_(self.u_p, gain=cfg.get('u_p', 0.5) * 10.0)
+            torch.nn.init.uniform_(self.u_p, 0, cfg.get('u_p', 0.5) * torch.sqrt(1 / torch.tensor(self.num_compartments)))
         else:
             self.u_p = cfg.get('u_p', 0.5)
-            
+
         self.weight = Parameter(
             torch.empty((self.out_features + self.out_features * self.num_compartments, self.in_features + self.in_features * self.num_compartments), **factory_kwargs)
         )
