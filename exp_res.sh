261e49b193a8d8301680f7ffe42f7cfa4bc34899
michael.siegl <michael.siegl@pro2future.at>
2025-12-28 13:02:20 +0000

    added single layer mclif - performance is worse, not unexpected

--- exp_res.sh
+++ exp_res.sh
@@ -6,7 +6,7 @@
 experiments=("SHD_M3CLIF2_S")
 runs=10
 gpu_count=1  # Number of available GPUs
-gpu_offset=6
+gpu_offset=0
 
 # no recurrent command
 # exp_name=SHD_LIF_2layer_norecurrent l1.use_recurrent=False l2.use_recurrent=False l1.n_neurons=590 l2.input_size=590 l2.n_neurons=590 l_out.input_size=590
@@ -16,9 +16,9 @@ for exp in "${experiments[@]}"; do
         gpu_id=$(( (i - 1) % gpu_count + gpu_offset ))
         echo "Running $exp - Iteration $i on GPU $gpu_id"
         if [[ $i -lt $runs ]]; then
-            uv run run.py experiment=$exp device=cuda:$gpu_id exp_name=SHD_MCLIF1L_updivided &
+            uv run run.py experiment=$exp device=cuda:$gpu_id exp_name=SHD_MCLIF1L_6compartments &
         else
-            uv run run.py experiment=$exp device=cuda:$gpu_id exp_name=SHD_MCLIF1L_updivided
+            uv run run.py experiment=$exp device=cuda:$gpu_id exp_name=SHD_MCLIF1L_6compartments
         fi
     done
 done
--- module/tau_monitor.py
+++ module/tau_monitor.py
@@ -8,7 +8,8 @@ class TauMonitor(pl.Callback):
                 tau_value = getattr(pl_module.l1, f"{tau}_trainer").get_tau().detach()
                 pl_module.log(f"l1_{tau}_mean", tau_value.mean().item(), prog_bar=False)
                 pl_module.log(f"l1_{tau}_std", tau_value.std().item(), prog_bar=False)
-            if hasattr(pl_module.l2, f"{tau}_trainer"):
-                tau_value = getattr(pl_module.l2, f"{tau}_trainer").get_tau().detach()
-                pl_module.log(f"l2_{tau}_mean", tau_value.mean().item(), prog_bar=False)
-                pl_module.log(f"l2_{tau}_std", tau_value.std().item(), prog_bar=False)
+            if hasattr(pl_module, "l2"):
+                if hasattr(pl_module.l2, f"{tau}_trainer"):
+                    tau_value = getattr(pl_module.l2, f"{tau}_trainer").get_tau().detach()
+                    pl_module.log(f"l2_{tau}_mean", tau_value.mean().item(), prog_bar=False)
+                    pl_module.log(f"l2_{tau}_std", tau_value.std().item(), prog_bar=False)
