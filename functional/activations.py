72b8e476c024ccd185876639d16472dd9cd9e186
michael.siegl <michael.siegl@pro2future.at>
2026-01-02 19:13:37 +0000

    added slayer function

--- functional/activations.py
+++ functional/activations.py
@@ -18,3 +18,20 @@ class FastSigmoid(torch.autograd.Function):
         grad_input = grad_output.clone()
         grad = grad_input / (ctx.k * torch.abs(mem) + 1.0) ** 2  # gradient of fast sigmoid on backward pass: Eq(4)
         return grad, None
+    
+@staticmethod
+class SLAYER(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, mem, alpha=2.0, c=0.5):
+        ctx.save_for_backward(mem)
+        ctx.alpha = alpha
+        ctx.c = c
+        out = (mem > 0).float()
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        (mem,) = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad = grad_input * (ctx.c * ctx.alpha) / (2 * torch.exp(ctx.alpha * torch.abs(mem)))
+        return grad, None, None
