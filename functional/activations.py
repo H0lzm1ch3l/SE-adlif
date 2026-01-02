f8ef9ffde762f264135efc877565c129637f6cce
michael.siegl <michael.siegl@pro2future.at>
2026-01-02 19:10:56 +0000

    add custom activation functions

---
+++ functional/activations.py
@@ -0,0 +1,20 @@
+
+import torch
+
+# Forward pass: Heaviside function
+# Backward pass: Override Dirac Delta with gradient of fast sigmoid
+@staticmethod
+class FastSigmoid(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, mem, k=25):
+        ctx.save_for_backward(mem) # store the membrane potential for use in the backward pass
+        ctx.k = k
+        out = (mem > 0).float() # Heaviside on the forward pass: Eq(1)
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        (mem,) = ctx.saved_tensors  # retrieve membrane potential
+        grad_input = grad_output.clone()
+        grad = grad_input / (ctx.k * torch.abs(mem) + 1.0) ** 2  # gradient of fast sigmoid on backward pass: Eq(4)
+        return grad, None
