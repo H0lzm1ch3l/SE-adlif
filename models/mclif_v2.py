a1b8112e8be5f020a5fb6ebca23f210b501f0751
michael.siegl <michael.siegl@pro2future.at>
2026-03-15 12:46:51 +0000

    yeah ok now I got it back, u_d and d have to be decoupled but I can ditch the dendritic recurrency mask, nice

--- models/mclif_v2.py
+++ models/mclif_v2.py
@@ -145,7 +145,7 @@ class MCLIF2(Module):
                 # cur = cur + cur_rec_s.reshape(-1, self.num_out_neuron, self.num_compartments)
                 
             if self.recurrent_dendrite:
-                cur_rec_d = torch.einsum('bni,nji->bnj', p_tm1, self.dendritic_recurrency_mask * self.dendritic_recurrent)
+                cur_rec_d = torch.einsum('bni,nji->bnj', p_tm1, self.dendritic_recurrent)
                 d_cur = d_cur + cur_rec_d
                 
             d = beta * d_tm1 + (1-beta) * d_cur
@@ -153,9 +153,9 @@ class MCLIF2(Module):
             d = d * (1 - p.detach()) + (d_rest * p.detach())
             t = gamma * t_tm1 + (1-gamma) * p
             active_dendrite = SLAYER.apply(t - self.epsilon, self.alpha, self.c)
-            d = (active_dendrite * self.u_p) + d
+            dendritic_influx = (active_dendrite * self.u_p) + d
                     
-            u = alpha * u_tm1 + (1-alpha) * (s_cur + d.sum(-1))
+            u = alpha * u_tm1 + (1-alpha) * (s_cur + dendritic_influx.sum(-1))
             z = SLAYER.apply(u - s_thr, self.alpha, self.c)
             u = u * (1 - z.detach()) + u_rest * z.detach()
             
